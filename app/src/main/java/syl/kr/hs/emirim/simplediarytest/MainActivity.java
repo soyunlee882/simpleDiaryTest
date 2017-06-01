2eeea8dd11c9d28c4d328e5a5dae0cde87fe2247
soyunlee882 <soyunlee882@gmail.com>
2017-06-01 19:27:55 +0900

    #5 Button progarmming push

--- app/src/main/java/syl/kr/hs/emirim/simplediarytest/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/simplediarytest/MainActivity.java
@@ -1,14 +1,18 @@
 package syl.kr.hs.emirim.simplediarytest;
 
+import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -25,6 +29,22 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.date_pick);
         edit = (EditText) findViewById(R.id.edit);
         but = (Button) findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str=edit.getText().toString();
+                    fOut.write(str.getBytes());
+                    fOut.close();
+                    Toast.makeText(MainActivity.this,"정삭적으로"+fileName+"파일이 저장되었습니다.",Toast.LENGTH_LONG).show();
+
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
         //캔린더 설정시 java용으로 설정
         Calendar cal=Calendar.getInstance();
         int year=cal.get(Calendar.YEAR);
@@ -56,15 +76,23 @@ public class MainActivity extends AppCompatActivity {
             diaryStr=new String(buf).trim();
            //trim() :: 앞뒤 공백 넣어 주기;
             but.setText("수정 하기");
+           //fIn.close();
         }catch(FileNotFoundException e){
          //   e.printStackTrace();
             edit.setText("일기가 존재하지 않습니다.");
             but.setText("새로 저장");
+
         } catch (IOException e) {
-            //e.printStackTrace();
-
-
+            e.printStackTrace();
         }
+        try{
+            fIn.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         return diaryStr;
     }
 }
